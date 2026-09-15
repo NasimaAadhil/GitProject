b91a614295d906abf49be9b832910e2fe895efb4
NasimaAadhil <aadhilnasi@gmail.com>
2026-09-15 16:22:41 +0530

    updated and created

--- Sample.java
+++ Sample.java
@@ -5,9 +5,13 @@ public class Employee{
   public void empId(){
     System.out.println("Employee Id : 210");
   }
+  public void empAddress(){
+    System.out.println("Employee Address : Dhaka");
+  }
   public static void main(String[] args){
     Employee e = new Employee();
-    e.empName;
-    e.empId;
+    e.empName();
+    e.empId();
+    e.empAddress();
   }
 }
