2762c6f383085ac479a5b0e178e51099e51e1bdd
NasimaAadhil <aadhilnasi@gmail.com>
2026-09-15 16:06:57 +0530

    add main method in Sample.java

--- Sample.java
+++ Sample.java
@@ -5,4 +5,9 @@ public class Employee{
   public void empId(){
     System.out.println("Employee Id : 210");
   }
+  public static void main(String[] args){
+    Employee e = new Employee();
+    e.empName;
+    e.empId;
+  }
 }
